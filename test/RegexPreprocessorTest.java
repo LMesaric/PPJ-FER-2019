9ff8197a6fcb4c3a02ce751954d37330abb2b29d
yatsukha <yatsukha.roman@gmail.com>
2019-10-07 11:58:46 +0200

    Add a test case with a leading \{ before the variable

--- test/RegexPreprocessorTest.java
+++ test/RegexPreprocessorTest.java
@@ -33,6 +33,19 @@ public class RegexPreprocessorTest {
             )
         );
 
+        assertArrayEquals(
+            new String[]{
+                "{a} elderberries",
+                "{b} \\{|(elderberries)"
+            },
+            new RegexPreprocessor().parse(
+                new String[]{
+                    "{a} elderberries",
+                    "{b} \\{|{a}"
+                }
+            )
+        );
+
         assertEquals(
             "{znamenka} 0|1|2|3|4|5|6|7|8|9\n" +
             "{hexZnamenka} (0|1|2|3|4|5|6|7|8|9)|a|b|c|d|e|f|A|B|C|D|E|F\n" +
