a70d06996f03ec71db9263cd2e4dc93bcb74f577
Filip Husnjak <filip.husnjak@fer.hr>
2019-10-13 16:22:37 +0200

    Add test for comments

--- src/test/java/EnkaTest.java
+++ src/test/java/EnkaTest.java
@@ -49,6 +49,8 @@ class EnkaTest {
         assertEquals(testEnka("1*2*", "22"), EnkaStatus.ACCEPTED);
         assertEquals(testEnka("1*2*", "1"), EnkaStatus.ACCEPTED);
         assertEquals(testEnka("1*2*", "2"), EnkaStatus.ACCEPTED);
+
+        assertEquals(testEnka("\\(|\\)|\\{|\\}|\\||\\*|\\\\|\\$|\\t|\\n|\\_|!|\"|#|%|&|'|+|,|-|.|/|0|1|2|3|4|5|6|7|8|9|:|;|<|=|>|?|@|A|B|C|D|E|F|G|H|I|J|K|L|M|N|O|P|Q|R|S|T|U|V|W|X|Y|Z|[|]|^|_|`|a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z|~", "|"), EnkaStatus.ACCEPTED);
     }
 
     private EnkaStatus testEnka(String expression, String simulate) throws IOException {
