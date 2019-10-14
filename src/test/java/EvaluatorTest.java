d1d95db8105cbf8a56d33e50130bb54e38f62f50
pmatosevic <patrik.matosevic1@gmail.com>
2019-10-14 22:40:31 +0200

    Add measuring time to SPRUT Evaluator

--- src/test/java/EvaluatorTest.java
+++ src/test/java/EvaluatorTest.java
@@ -22,9 +22,21 @@ class EvaluatorTest {
 
     private static final String TESTCASES_DIR = "src/test/resources/testcases_GLA_LA";
 
+    public static void main(String[] args) throws IOException, InterruptedException {
+        System.out.println("SPRUT evaluator v1.0");
+        System.out.println("Starting tests");
+        System.out.println();
+        testAll(false);
+    }
+
     @Test
     void test() throws IOException, InterruptedException {
+        testAll(true);
+    }
+
+    private static void testAll(boolean runFromJunit) throws IOException, InterruptedException {
         Path testsDir = Paths.get(TESTCASES_DIR);
+        int failed = 0;
         for (Path test : Files.newDirectoryStream(testsDir)) {
             Path input = null;
             Path output = null;
@@ -36,8 +48,11 @@ class EvaluatorTest {
                 if (fileName.endsWith(".out")) output = file;
             }
 
-            assertTrue(evaluateTestCase(definition, input, output));
+            boolean result = evaluateTestCase(test.getFileName().toString(), definition, input, output, runFromJunit);
+            if (!result) failed++;
+            if (runFromJunit) assertTrue(result);
         }
+        if (!runFromJunit) System.out.println("Failed: " + failed + " tests.");
     }
 
     private static void prepareProcess(Path path, Process process) throws IOException {
@@ -50,35 +65,65 @@ class EvaluatorTest {
         }
     }
 
-    private static boolean evaluateTestCase(Path definition, Path input, Path output) throws IOException, InterruptedException {
+    private static List<String> inputStreamToLines(InputStream is) throws IOException {
+        List<String> lines = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(is))) {
+            while (true) {
+                String line = reader.readLine();
+                if (line == null) break;
+                lines.add(line);
+            }
+        }
+        return lines;
+    }
+
+    private static boolean evaluateTestCase(String testName, Path definition, Path input, Path output, boolean runFromJunit)
+            throws IOException, InterruptedException {
         ProcessBuilder genBuilder = new ProcessBuilder()
                 .command(JAVA_EXEC, JAVA_PARAMS1, JAVA_PARAMS2, GENERATOR_CLASS);
 
+        long generatorStart = System.nanoTime();
         Process generator = genBuilder.start();
         prepareProcess(definition, generator);
         generator.getOutputStream().close();
         generator.waitFor();
+        long generatorEnd = System.nanoTime();
 
         ProcessBuilder laBuilder = new ProcessBuilder()
                 .command(JAVA_EXEC, JAVA_PARAMS1, JAVA_PARAMS2, ANALYZER_CLASS)
                 .redirectErrorStream(false);
 
+        long analyzerStart = System.nanoTime();
         Process analyzer = laBuilder.start();
         prepareProcess(input, analyzer);
         analyzer.getOutputStream().close();
 
         List<String> expected = Files.readAllLines(output);
-        List<String> actual = new ArrayList<>();
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(analyzer.getInputStream()))) {
-            while (true) {
-                String line = reader.readLine();
-                if (line == null) break;
-                actual.add(line);
+        List<String> stdout = inputStreamToLines(analyzer.getInputStream());
+        List<String> stderr = inputStreamToLines(analyzer.getErrorStream());
+        analyzer.waitFor();
+        long analyzerEnd = System.nanoTime();
+
+        boolean isExpected = expected.equals(stdout);
+
+        if (!runFromJunit) {
+            System.out.println("--------------------");
+            System.out.printf("TEST %s: %s%n", testName, isExpected ? "PASS" : "FAIL");
+            System.out.printf("Generator time: %.3f s%n", (generatorEnd - generatorStart) / 1000000000.0);
+            System.out.printf("Analyzer time: %.3f s%n", (analyzerEnd - analyzerStart) / 1000000000.0);
+            //System.out.println("Analyzer stderr:");
+            //stderr.forEach(s -> System.out.println("\t" + s));
+            if (!isExpected) {
+                System.out.println("Expected:");
+                expected.forEach(s -> System.out.println("\t" + s));
+                System.out.println("Actual:");
+                stdout.forEach(s -> System.out.println("\t" + s));
             }
-            analyzer.waitFor();
+            System.out.println("--------------------");
+            System.out.println();
         }
 
-        return expected.equals(actual);
+        return isExpected;
     }
 
 }
