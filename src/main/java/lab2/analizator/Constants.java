c2c180b33eadb62b0043982f2c1f366db39fe533
LMesaric <luka.mesaric@fer.hr>
2019-11-08 20:55:31 +0100

    Add constants

---
+++ src/main/java/lab2/analizator/Constants.java
@@ -0,0 +1,6 @@
+package lab2.analizator;
+
+// Two exact copies of Constants class exist purely because of the way online evaluator is set up.
+class Constants {
+    static final char EPSILON = '$';
+}
