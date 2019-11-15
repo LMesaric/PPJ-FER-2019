a7d19b7c22cfe039cb7369de07d2a842da3c7ca8
LMesaric <luka.mesaric@fer.hr>
2019-11-15 20:13:41 +0100

    Add serialization path constants

--- src/main/java/lab2/Constants.java
+++ src/main/java/lab2/Constants.java
@@ -2,6 +2,10 @@ package lab2;
 
 // Two exact copies of Constants class exist purely because of the way online evaluator is set up.
 class Constants {
+    static final String ACTION_TABLE_PATH = "./src/main/java/lab2/analizator/action.ser";
+
+    static final String NEW_STATE_TABLE_PATH = "./src/main/java/lab2/analizator/newState.ser";
+
     static final String EPSILON = "$";
 
     static final String INITIAL_STATE = "S\'";
--- src/main/java/lab2/analizator/Constants.java
+++ src/main/java/lab2/analizator/Constants.java
@@ -2,5 +2,9 @@ package lab2.analizator;
 
 // Two exact copies of Constants class exist purely because of the way online evaluator is set up.
 class Constants {
+    static final String ACTION_TABLE_PATH = "./src/main/java/lab2/analizator/action.ser";
+
+    static final String NEW_STATE_TABLE_PATH = "./src/main/java/lab2/analizator/newState.ser";
+
     static final char EPSILON = '$';
 }
