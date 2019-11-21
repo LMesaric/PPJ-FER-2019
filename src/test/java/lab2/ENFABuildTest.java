5f8f8701df651d5c6f567ef7eb3f663682062386
Filip Husnjak <filip.husnjak@fer.hr>
2019-11-21 22:56:20 +0100

    Extract ENFA creation to Util class

--- src/test/java/lab2/ENFABuildTest.java
+++ src/test/java/lab2/ENFABuildTest.java
@@ -8,13 +8,10 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
 
-import static lab2.Constants.EPSILON;
-import static lab2.Constants.INITIAL_STATE;
+import static lab2.TestUtil.TESTCASES_DIR_ENFA;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ENFABuildTest {
-
-    private static final String TESTCASES_DIR = "src/test/resources/testcases_ENKA/";
 
     @Test
     void test() throws IOException {
@@ -23,69 +20,20 @@ class ENFABuildTest {
     }
 
     private boolean test(String fileName) throws IOException {
-        Path pathIn = Paths.get(TESTCASES_DIR + fileName + ".in");
-        Path pathOut = Paths.get(TESTCASES_DIR + fileName + ".out");
-
-        String[] readInput = new String(Files.readAllBytes(pathIn)).split("\n");
-        Set<String> nonterminalSymbols = cleanSymbolInput(readInput[0]);
-        Set<String> terminalSymbols = cleanSymbolInput(readInput[1]);
-        Set<String> synchronizationalSymbols = cleanSymbolInput(readInput[2]);
-        Map<String, List<List<String>>> productions = parseProductions(readInput, 3);
-        nonterminalSymbols.add(INITIAL_STATE);
-        String start = readInput[0].split(" ")[1];
-        List<List<String>> production = new LinkedList<>();
-        production.add(Collections.singletonList(start));
-        productions.put(INITIAL_STATE, production);
-
-        String s = new ENFA(INITIAL_STATE, productions, nonterminalSymbols).build().print();
+        Path pathOut = Paths.get(TESTCASES_DIR_ENFA + fileName + ".out");
+        String s = TestUtil.createENFA(fileName).print();
         String[] arr1 = s.split("\n");
         String[] arr2 = new String(Files.readAllBytes(pathOut)).split("\n");
         for (int i = 0; i < arr2.length; i++) {
             arr2[i] = arr2[i].replace("\r", "");
         }
-
         for (int i = 0; i < arr1.length && i < arr2.length; i++) {
             if (!arr1[i].equals(arr2[i])) {
                 System.out.println("Arr1 (" + i + ") :" + arr1[i]);
                 System.out.println("Arr2 (" + i + ") :" + arr2[i]);
             }
         }
-
         return Arrays.equals(arr1, arr2);
-    }
-
-    private static Set<String> cleanSymbolInput(String line) {
-        String[] tmp = line.trim().split(" ");
-        Set<String> set = new HashSet<>();
-        for (int i = 1, limit = tmp.length; i < limit; i++)
-            set.add(tmp[i]);
-        return set;
-    }
-
-    private static Map<String, List<List<String>>> parseProductions(
-            String[] input,
-            @SuppressWarnings("SameParameterValue") int offset
-    ) {
-        Map<String, List<List<String>>> map = new HashMap<>();
-        int i = offset, last = input.length;
-        while (i < last) {
-            String nonterminal = input[i++].trim();
-            List<List<String>> rightSides = new ArrayList<>();
-            while (i < last && input[i].charAt(0) == ' ') {
-                if (input[i].trim().equals(EPSILON)) {
-                    rightSides.add(Collections.emptyList());
-                } else {
-                    rightSides.add(Arrays.asList(input[i].trim().split(" ")));
-                }
-                i++;
-            }
-            if (map.containsKey(nonterminal)) {
-                map.get(nonterminal).addAll(rightSides);
-            } else {
-                map.put(nonterminal, rightSides);
-            }
-        }
-        return map;
     }
 
 }
