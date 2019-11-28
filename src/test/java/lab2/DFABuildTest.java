0f82ad1300fefa28ef8bf48635a5660079cac350
LMesaric <luka.mesaric@fer.hr>
2019-11-28 18:09:22 +0100

    Clean up imports

--- src/test/java/lab2/DFABuildTest.java
+++ src/test/java/lab2/DFABuildTest.java
@@ -9,7 +9,6 @@ import java.nio.file.Paths;
 import java.util.Arrays;
 
 import static lab2.TestUtil.TESTCASES_DIR_DFA;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class DFABuildTest {
 
--- src/test/java/lab2/ENFABuildTest.java
+++ src/test/java/lab2/ENFABuildTest.java
@@ -6,10 +6,10 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.*;
+import java.util.Arrays;
 
 import static lab2.TestUtil.TESTCASES_DIR_ENFA;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ENFABuildTest {
 
