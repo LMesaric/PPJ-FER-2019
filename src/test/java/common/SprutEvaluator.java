435320de9536d8057aad53336c428bc736ffe9d6
pmatosevic <patrik.matosevic1@gmail.com>
2019-11-29 19:28:32 +0100

    Add new Sprut Evaluator for all labs

---
+++ src/test/java/common/SprutEvaluator.java
@@ -0,0 +1,149 @@
+package common;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
+
+public class SprutEvaluator {
+
+    private static final String JAVA_EXEC = "java";
+    private static final String JAVA_PARAMS1 = "-cp";
+    private static final String JAVA_PARAMS2 = "target/classes";
+    private static final int BUFFER_LENGTH = 1024;
+    private static final int MAX_TIMEOUT_MS = 10000;
+
+    private Consumer<String> outputConsumer;
+    private Consumer<String> errorConsumer;
+    private Path testsDirectory;
+    private String generatorClass;
+    private String analyzerClass;
+    private String inputRegex;
+    private String definitionRegex;
+    private String outputRegex;
+
+    public SprutEvaluator(Consumer<String> outputConsumer, String testsDirectory,
+                          String generatorClass, String analyzerClass,
+                          String inputRegex, String definitionRegex, String outputRegex) {
+        this.outputConsumer = outputConsumer;
+        this.testsDirectory = Paths.get(testsDirectory);
+        this.generatorClass = generatorClass;
+        this.analyzerClass = analyzerClass;
+        this.inputRegex = inputRegex;
+        this.definitionRegex = definitionRegex;
+        this.outputRegex = outputRegex;
+    }
+
+    public boolean evaluateAll() throws IOException, InterruptedException {
+        int testCounter = 0;
+        int passCounter = 0;
+        for (Path caseDir : Files.newDirectoryStream(testsDirectory)) {
+            String caseName = caseDir.getFileName().toString();
+            Path input = null;
+            Path expected = null;
+            Path definition = null;
+            for (Path file : Files.newDirectoryStream(caseDir)) {
+                String fileName = file.getFileName().toString();
+                if (fileName.matches(inputRegex)) input = file;
+                if (fileName.matches(definitionRegex)) definition = file;
+                if (fileName.matches(outputRegex)) expected = file;
+            }
+
+            if (input == null || expected == null || definition == null) {
+                print("Test %s does not have all required files!", caseName);
+                continue;
+            }
+
+            testCounter++;
+            boolean result = evaluateTestCase(caseName, definition, input, expected);
+            if (result) passCounter++;
+        }
+
+        print("Test result: %d/%d passed.", passCounter, testCounter);
+        return passCounter == testCounter;
+    }
+
+    private boolean evaluateTestCase(String caseName, Path definition, Path input, Path output)
+            throws IOException, InterruptedException {
+        ProcessBuilder genBuilder = new ProcessBuilder()
+                .command(JAVA_EXEC, JAVA_PARAMS1, JAVA_PARAMS2, generatorClass);
+
+        long generatorStart = System.nanoTime();
+        Process generator = genBuilder.start();
+        injectFileAsStdinToProcess(definition, generator);
+        generator.getOutputStream().close();
+        generator.waitFor();
+        long generatorEnd = System.nanoTime();
+
+        ProcessBuilder laBuilder = new ProcessBuilder()
+                .command(JAVA_EXEC, JAVA_PARAMS1, JAVA_PARAMS2, analyzerClass)
+                .redirectErrorStream(false);
+
+        long analyzerStart = System.nanoTime();
+        Process analyzer = laBuilder.start();
+        injectFileAsStdinToProcess(input, analyzer);
+        analyzer.getOutputStream().close();
+
+        List<String> expected = Files.readAllLines(output);
+        List<String> stdout = readLinesFromInputStream(analyzer.getInputStream());
+        List<String> stderr = readLinesFromInputStream(analyzer.getErrorStream());
+        analyzer.waitFor();
+        long analyzerEnd = System.nanoTime();
+
+        boolean isExpected = expected.equals(stdout);
+
+        print("--------------------");
+        print("TEST %s: %s", caseName, isExpected ? "PASS" : "FAIL");
+        print("Generator time: %.3f s", getTimeInSeconds(generatorStart, generatorEnd));
+        print("Analyzer time: %.3f s", getTimeInSeconds(analyzerStart, analyzerEnd));
+        //System.out.println("Analyzer stderr:");
+        //stderr.forEach(s -> print("\t" + s));
+        if (!isExpected) {
+            print("Expected:");
+            expected.forEach(s -> print("\t" + s));
+            System.out.println("Actual:");
+            stdout.forEach(s -> print("\t" + s));
+        }
+        print("--------------------");
+        print("");
+
+        return isExpected;
+    }
+
+    private void injectFileAsStdinToProcess(Path filePath, Process process) throws IOException {
+        try (InputStream is = Files.newInputStream(filePath)) {
+            byte[] buffer = new byte[BUFFER_LENGTH];
+            int len;
+            while ((len = is.read(buffer)) != -1) {
+                process.getOutputStream().write(buffer, 0, len);
+            }
+        }
+    }
+
+    private List<String> readLinesFromInputStream(InputStream is) throws IOException {
+        List<String> lines = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(is))) {
+            while (true) {
+                String line = reader.readLine();
+                if (line == null) break;
+                lines.add(line);
+            }
+        }
+        return lines;
+    }
+
+    private void print(String format, Object... args) {
+        outputConsumer.accept(String.format(format, args));
+    }
+
+    private double getTimeInSeconds(long measurementStart, long measurementEnd) {
+        return (measurementEnd - measurementStart) / 1000000000.0;
+    }
+}
--- src/test/java/lab2/Sprut.java
+++ src/test/java/lab2/Sprut.java
@@ -1,128 +1,19 @@
 package lab2;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
+import common.SprutEvaluator;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.io.IOException;
 
 public class Sprut {
 
-    private static final String JAVA_EXEC = "java";
-    private static final String JAVA_PARAMS1 = "-cp";
-    private static final String JAVA_PARAMS2 = "target/classes";
     private static final String GENERATOR_CLASS = "lab2.GSA";
     private static final String ANALYZER_CLASS = "lab2.analizator.SA";
-
     private static final String TESTCASES_DIR = "src/test/resources/testcases_GSA_SA";
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        System.out.println("SPRUT evaluator v1.0");
-        System.out.println("Starting tests");
-        System.out.println();
-
-        long testStart = System.nanoTime();
-        testAll(false);
-        long testEnd = System.nanoTime();
-        System.out.printf("Total time time: %.3f s%n", (testEnd - testStart) / 1000000000.0);
-    }
-
-    static void testAll(boolean runFromJunit) throws IOException, InterruptedException {
-        Path testsDir = Paths.get(TESTCASES_DIR);
-        int failed = 0;
-        for (Path test : Files.newDirectoryStream(testsDir)) {
-            Path input = null;
-            Path output = null;
-            Path definition = null;
-            for (Path file : Files.newDirectoryStream(test)) {
-                String fileName = file.getFileName().toString();
-                if (fileName.endsWith(".in")) input = file;
-                if (fileName.endsWith(".san")) definition = file;
-                if (fileName.endsWith(".out")) output = file;
-            }
-
-            boolean result = evaluateTestCase(test.getFileName().toString(), definition, input, output, runFromJunit);
-            if (!result) failed++;
-            if (runFromJunit) assertTrue(result);
-        }
-        if (!runFromJunit) System.out.println("Failed: " + failed + " tests.");
-    }
-
-    private static void prepareProcess(Path path, Process process) throws IOException {
-        try (InputStream is = Files.newInputStream(path)) {
-            byte[] buffer = new byte[1024];
-            int len;
-            while ((len = is.read(buffer)) != -1) {
-                process.getOutputStream().write(buffer, 0, len);
-            }
-        }
-    }
-
-    private static List<String> inputStreamToLines(InputStream is) throws IOException {
-        List<String> lines = new ArrayList<>();
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(is))) {
-            while (true) {
-                String line = reader.readLine();
-                if (line == null) break;
-                lines.add(line);
-            }
-        }
-        return lines;
-    }
-
-    private static boolean evaluateTestCase(String testName, Path definition, Path input, Path output, boolean runFromJunit)
-            throws IOException, InterruptedException {
-        ProcessBuilder genBuilder = new ProcessBuilder()
-                .command(JAVA_EXEC, JAVA_PARAMS1, JAVA_PARAMS2, GENERATOR_CLASS);
-
-        long generatorStart = System.nanoTime();
-        Process generator = genBuilder.start();
-        prepareProcess(definition, generator);
-        generator.getOutputStream().close();
-        generator.waitFor();
-        long generatorEnd = System.nanoTime();
-
-        ProcessBuilder laBuilder = new ProcessBuilder()
-                .command(JAVA_EXEC, JAVA_PARAMS1, JAVA_PARAMS2, ANALYZER_CLASS)
-                .redirectErrorStream(false);
-
-        long analyzerStart = System.nanoTime();
-        Process analyzer = laBuilder.start();
-        prepareProcess(input, analyzer);
-        analyzer.getOutputStream().close();
-
-        List<String> expected = Files.readAllLines(output);
-        List<String> stdout = inputStreamToLines(analyzer.getInputStream());
-        List<String> stderr = inputStreamToLines(analyzer.getErrorStream());
-        analyzer.waitFor();
-        long analyzerEnd = System.nanoTime();
-
-        boolean isExpected = expected.equals(stdout);
-
-        if (!runFromJunit) {
-            System.out.println("--------------------");
-            System.out.printf("TEST %s: %s%n", testName, isExpected ? "PASS" : "FAIL");
-            System.out.printf("Generator time: %.3f s%n", (generatorEnd - generatorStart) / 1000000000.0);
-            System.out.printf("Analyzer time: %.3f s%n", (analyzerEnd - analyzerStart) / 1000000000.0);
-            //System.out.println("Analyzer stderr:");
-            //stderr.forEach(s -> System.out.println("\t" + s));
-            if (!isExpected) {
-                System.out.println("Expected:");
-                expected.forEach(s -> System.out.println("\t" + s));
-                System.out.println("Actual:");
-                stdout.forEach(s -> System.out.println("\t" + s));
-            }
-            System.out.println("--------------------");
-            System.out.println();
-        }
-
-        return isExpected;
+        SprutEvaluator sprut = new SprutEvaluator(System.out::println, TESTCASES_DIR, GENERATOR_CLASS, ANALYZER_CLASS,
+                ".*\\.in", ".*\\.san", ".*\\.out");
+        sprut.evaluateAll();
     }
 
 }
