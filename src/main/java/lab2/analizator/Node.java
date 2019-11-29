ec78e17ebc35c2d866fa9e651135542301f5386a
LMesaric <luka.mesaric@fer.hr>
2019-11-29 18:29:18 +0100

    Add syntax tree node

---
+++ src/main/java/lab2/analizator/Node.java
@@ -0,0 +1,29 @@
+package lab2.analizator;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+
+class Node {
+
+    final String text;
+    final List<Node> children;
+
+    Node(String text) {
+        this(text, new LinkedList<>());
+    }
+
+    Node(String text, List<Node> children) {
+        this.text = Objects.requireNonNull(text);
+        this.children = Objects.requireNonNull(children);
+    }
+
+    void addChildFirst(Node child) {
+        children.add(0, Objects.requireNonNull(child));
+    }
+
+    void addChildLast(Node child) {
+        children.add(Objects.requireNonNull(child));
+    }
+
+}
