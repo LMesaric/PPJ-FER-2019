3a85a40904e84008bfd9239c9dbdacf14fbb6677
LMesaric <luka.mesaric@fer.hr>
2019-12-02 09:18:19 +0100

    Use new Sprut version for testing lab1

--- src/test/java/lab1/EvaluatorTest.java
+++ src/test/java/lab1/EvaluatorTest.java
@@ -1,14 +1,20 @@
 package lab1;
 
+import common.SprutEvaluator;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+
+import static lab1.Sprut.*;
 
 class EvaluatorTest {
 
     @Test
     void test() throws IOException, InterruptedException {
-        Sprut.testAll(true);
+        SprutEvaluator sprut = new SprutEvaluator(str -> {}, TESTCASES_DIR, GENERATOR_CLASS, ANALYZER_CLASS,
+                ".*\\.in", ".*\\.lan", ".*\\.out");
+        Assertions.assertTrue(sprut.evaluateAll());
     }
 
 }
