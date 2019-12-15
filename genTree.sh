0eb3feed29ad9d151cefc09672ba61b77e51832d
yatsukha <yatsukha.roman@gmail.com>
2019-12-15 21:46:25 +0100

    Add a script for generating parse tree from arbitrary input file

---
+++ genTree.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# read from stdin
+src='cat'
+
+# read from file
+if [[ $# -eq 1 ]]; then
+  src="cat $1"
+fi
+
+# assert cd is the location of the script
+cd "${0%/*}"
+
+run='java -cp target/classes'
+base='src/main/resources'
+
+# lexer definitions
+cat $base/ppjC.lan | $run lab1.GLA && \
+  # lexer parsing
+  $src | ($run lab1.analizator.LA && \
+  # syntax definitons
+  cat $base/ppjC.san | $run lab2.GSA) | \
+  # syntax parsing
+  $run lab2.analizator.SA
