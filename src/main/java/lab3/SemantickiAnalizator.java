c3bd31bb178be8a712b422b1c3537b314fe07406
Filip Husnjak <filip.husnjak@fer.hr>
2019-12-21 16:49:35 +0100

    Create enum for type and class which wraps type and constant flag

--- src/main/java/lab3/SemantickiAnalizator.java
+++ src/main/java/lab3/SemantickiAnalizator.java
@@ -3,8 +3,15 @@ package lab3;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 public class SemantickiAnalizator {
+
+    int currentScope = 0;
+
+    enum Type {
+        VOID, CHAR, INT
+    }
 
     public static void main(String[] args) {
         String inputText = new String(readAllFromStdin(), StandardCharsets.UTF_8);
@@ -13,7 +20,94 @@ public class SemantickiAnalizator {
         //TODO Epsilon production has a node with text set to "$" (Constants.EPSILON)
     }
 
-    static Node buildTree(String[] lines) {
+    private static void processCompileUnit(Node node) {
+        for (Node child: node.children) {
+            switch (node.elements.get(0)) {
+                case "<prijevodna_jedinica>":
+                    processCompileUnit(child);
+                    break;
+                case "<vanjska_deklaracija>":
+                    processOuterDeclaration(child);
+                    break;
+            }
+        }
+    }
+
+    private static void processOuterDeclaration(Node node) {
+        for (Node child: node.children) {
+            switch (node.elements.get(0)) {
+                case "<definicija_funkcije>":
+                    processFunctionDefinition(child);
+                    break;
+                case "<deklaracija>":
+                    processDeclaration(child);
+                    break;
+            }
+        }
+    }
+
+    private static void processFunctionDefinition(Node node) {
+        int typeDefPos;
+        String functionName;
+        TypeWrapper type;
+        List<Type> parameters = null;
+        for (Node child: node.children) {
+            switch (node.elements.get(0)) {
+                case "<ime_tipa>":
+                    type = typeName(child);
+                    break;
+                case "<IDN>":
+                    functionName = node.elements.get(2);
+                    break;
+                case "<lista_parametara>":
+
+                    break;
+                case "<slozena_naredba>":
+                    break;
+            }
+        }
+    }
+
+    private static TypeWrapper typeName(Node node) {
+        boolean constant = false;
+        Type type = null;
+        for (Node child: node.children) {
+            switch (child.elements.get(0)) {
+                case "KR_CONST":
+                    constant = true;
+                    break;
+                case "<specifikator_tipa>":
+                    type = typeSpecifier(child);
+                    break;
+            }
+        }
+        return new TypeWrapper(constant, type);
+    }
+
+    private static Type typeSpecifier(Node node) {
+        switch (node.elements.get(0)) {
+            case "KR_VOID":
+                return Type.VOID;
+            case "KR_CHAR":
+                return Type.CHAR;
+            default:
+                return Type.INT;
+        }
+    }
+
+    private static void processDeclaration(Node node) {
+        for (Node child: node.children) {
+            switch (node.elements.get(0)) {
+                case "<definicija_funkcije>":
+                    processCompileUnit(child);
+                    break;
+                case "<deklaracija>":
+                    processOuterDeclaration(child);
+            }
+        }
+    }
+
+    private static Node buildTree(String[] lines) {
         if (lines.length == 0 || lines.length == 1 && lines[0].trim().length() == 0)
             return null;
 
@@ -56,6 +150,16 @@ public class SemantickiAnalizator {
             return baos.toByteArray();
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    private static class TypeWrapper {
+        boolean constant;
+        Type type;
+
+        TypeWrapper(boolean constant, Type type) {
+            this.constant = constant;
+            this.type = type;
         }
     }
 
