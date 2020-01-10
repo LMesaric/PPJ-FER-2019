60966a7e3f23c8a70952b5d9d1626a982f417f9e
LMesaric <luka.mesaric@fer.hr>
2020-01-10 22:21:21 +0100

    Fix lab4 JUnit test assertion

--- src/test/java/lab4/EvaluatorTest.java
+++ src/test/java/lab4/EvaluatorTest.java
@@ -1,6 +1,7 @@
 package lab4;
 
 import common.SprutEvaluator;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -8,13 +9,13 @@ import java.io.IOException;
 import static lab4.Sprut.GENERATOR_CLASS;
 import static lab4.Sprut.TESTCASES_DIR;
 
-public class EvaluatorTest {
+class EvaluatorTest {
 
     @Test
-    public void test() throws IOException, InterruptedException {
+    void test() throws IOException, InterruptedException {
         SprutEvaluator sprut = new SprutEvaluator(TESTCASES_DIR, GENERATOR_CLASS,
-                ".*\\.in", ".*\\.out", System.out::println);
-        sprut.evaluateAll();
+                ".*\\.in", ".*\\.out", str -> {});
+        Assertions.assertTrue(sprut.evaluateAll());
     }
 
 }
--- src/test/java/lab4/Sprut.java
+++ src/test/java/lab4/Sprut.java
@@ -15,5 +15,4 @@ public class Sprut {
         sprut.evaluateAll();
     }
 
-
 }
